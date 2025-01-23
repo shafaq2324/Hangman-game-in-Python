f87e3535d9b927d40dae3e0c26236b8f387ec926
shafaq2324 <167637815+shafaq2324@users.noreply.github.com>
2025-01-23 20:00:26 +0530

    Create words.py

---
+++ words.py
@@ -0,0 +1,22 @@
+word_list = [
+    "python",
+    "hangman",
+    "developer",
+    "keyboard",
+    "programming",
+    "internet",
+    "variable",
+    "function",
+    "algorithm",
+    "computer",
+    "software",
+    "hardware",
+    "monitor",
+    "database",
+    "application",
+    "framework",
+    "syntax",
+    "debugging",
+    "project",
+    "code"
+]
